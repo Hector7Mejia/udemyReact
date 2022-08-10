4e192c4df3b36970bed2cd92a355156d81e31912
Hector Mejia <mejia7hector@gmail.com>
2022-08-10 18:12:04 -0500

    expenseFilter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,15 @@
-import  './ExpenseItem.css';
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import {useState} from "react";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    //useState(props.whatever) will always give an array with the current props and the updating function.
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = () => {
-        //no longer using = assignment while using useState()
-        // title = "updated;"
-        setTitle('Updated');
-    };
-
-    return (//jsx requires one overarching div as opposed to multiple root level divs
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+const ExpenseItem = (props) => {
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
