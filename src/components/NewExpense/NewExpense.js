1d51b29312751e128020d09cab5cf7458001255f
Hector Mejia <mejia7hector@gmail.com>
2022-08-10 14:19:09 -0500

    expense js

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,13 @@
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
     const saveExpenseData = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
+        props.onAddExpense(expenseData);
     };
     return(<div className="new-expense">
         <ExpenseForm onSaveExpenseData={saveExpenseData}/>
